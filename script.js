fec2e1aaa3e9f81adc09900b62b36c06a9df9105
Susan Roodsari <sroodsari@berkeley.edu>
2024-03-13 09:48:32 -0700

    updated

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
     var now = new Date(); // Get the current time
-    var hour = now.getHours(); // Extract the hours
+    var month = now.getMonth(); // get current month
+    var date = now.getDate(); // get current date
+    var hour = now.getHours(); // Extract the hour
+    var minutes = now.getMinutes(); // Extract the minutes
 
-    var mainHeader = document.getElementById('mainHeader'); // Get the message div
+    var contentDiv = document.getElementById('content'); // Get the content div
 
-    if (hour === 15) { // Check if it's 1 PM (13:00 in 24-hour format)
-        // Do something at 1 PM
-        mainHeader.innerHTML = "It's 1 PM! Time for something special.";
-    } else {
-        // Do something any other time
-        mainHeader = "It's not 1 PM. Just a regular time.";
+    // Specify the exact time you want the image to show (e.g., 13:30)
+    if(month === 2 && date === 14){
+        if ((hour === 15 && minutes === 14)||(hour === 3 && minutes === 14)) {
+            // Display an image at 3:14 
+            contentDiv.innerHTML = '<img src="./assets/Pie.png" alt="Special Image">';
+            contentDiv.innerHTML += '<div class="overlay">Happy Pi Day</div>';
+
+        } else {
+            // Display text at all other times
+            contentDiv.innerHTML = '<img src="./assets/Pie.png" alt="Special Image">';
+            
+        }
+    } else{
+        if ((hour === 15 && minutes === 14)||(hour === 3 && minutes === 14)) {
+            // Display an image at 3:14 
+            contentDiv.innerHTML = '<img src="./assets/Pie.png" alt="Special Image">';
+        } else {
+            // Display text at all other times
+            contentDiv.innerHTML = '<p>Come back at 3:14</p>';
+        }
+
     }
+    
 });
